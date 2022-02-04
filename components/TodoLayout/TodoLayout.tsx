374f13af2751919e69dd961813a593d03204740b
Yoshi <kawaii.yoshiyuki@gmail.com>
2022-02-04 12:43:53 +0100

    init props from home component

--- components/TodoLayout/TodoLayout.tsx
+++ components/TodoLayout/TodoLayout.tsx
@@ -3,7 +3,6 @@ import Instructions from './Instructions/Instructions';
 import TodoInput from './TodoInput/TodoInput';
 import TodoList from './TodoList/TodoList';
 
-
 type FormElem = React.FormEvent<HTMLFormElement>
 
 interface ITodo {
@@ -11,15 +10,9 @@ interface ITodo {
   complete: boolean,
 }
 
-const initState = [
-  { text: 'Fork repository', complete: false },
-  { text: 'Open build and deploy settings and connect', complete: false },
-  { text: 'Push a new commit', complete: false },
-];
-
-const TodoLayout = () => {
+const TodoLayout = ({ initTodos }: { initTodos: ITodo[] }) => {
     const [value, setValue] = useState<string>('');
-    const [todos, setTodos] =  useState<ITodo[]>(initState);
+    const [todos, setTodos] =  useState<ITodo[]>(initTodos);
 
     const addTodo = (text:string): void => {
       const newTodos: ITodo[] = [...todos, {text, complete: false}];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React  from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import TodoLayout from '../components/TodoLayout/TodoLayout';
 
-const Home = () => (
+const initState = [
+  { text: 'Fork repository', complete: false },
+  { text: 'Open build and deploy settings and connect', complete: false },
+  { text: 'Push a new commit', complete: false },
+];
+
+const Home = (props: any) => (
   <>
     <Header />
-    <TodoLayout />
+    <TodoLayout initTodos={props.data} />
     <Footer />
   </>
 );
 
+Home.getInitialProps = async () => ({ data: initState});
 
 export default Home;
